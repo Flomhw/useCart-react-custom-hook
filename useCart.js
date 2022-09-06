8331cc322de2b3f0594afab6db2055a202f5215c
Florian <33011758+Flomhw@users.noreply.github.com>
2022-09-06 09:25:53 +0200

    Add files via upload

---
+++ useCart.js
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+
+export default function useCart() {
+
+    const [cart, setCart] = useState([
+      {
+
+      }
+    ]); /* Here we store the product object */
+
+
+
+    const addItem = (productObj, quantity) =>{
+      !cart.includes(productObj) && setCart([...cart, {...productObj, quantity}]);
+    } /* Add to cart function */
+
+    const removeItem = productObj => {
+      setCart(cart.filter(
+        item => {
+          return item !== productObj;
+        }
+      ))
+    } /* Remove from cart function */
+
+    const quantityAdd = () => {
+
+    } /* Add one item function */
+
+    const quantityRemove = () => {
+
+    } /* Remove one item function */
+
+    const persistCart = savedCart => {
+      localStorage.setItem('oldCart', JSON.stringify(savedCart));
+    } /* Store cart info as local storage */
+
+    const getPersistedCart = () =>{
+      const getOldCart = localStorage.getItem('oldCart');
+      setCart(JSON.parse(getOldCart));
+    } /* Retrieve a previous cart from local storage */
+    
+    return {cart, setCart, addItem, removeItem, quantityAdd, quantityRemove, persistCart, getPersistedCart};
+}
